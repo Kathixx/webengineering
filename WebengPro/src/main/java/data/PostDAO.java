f4020aa5e7aa5c83a7569520752d2d6aaf9a17ef
victorialouise <Vivi0111@web.de>
2017-06-30 12:40:29 +0200

    posts
    posts liste

--- WebengPro/src/main/java/data/PostDAO.java
+++ WebengPro/src/main/java/data/PostDAO.java
@@ -1,5 +1,10 @@
 package data;
 
+import transfer.PostTransferObject;
+
 public interface PostDAO {
 
+	
+	/** Methode zum Auslesen aller Posts */
+	public List<PostTransferObject> getAllPosts();
 }
